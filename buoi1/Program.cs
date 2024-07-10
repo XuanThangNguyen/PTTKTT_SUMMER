07845197aae02692351559f9ee54eab4ed2f9c6d
XuanThangNguyen <thangprozzz123@gmail.com>
2024-07-10 11:15:42 +0700

    thêm code bai1 , ảnh debug bài mẫu và ảnh debug bai1

--- buoi1/Program.cs
+++ buoi1/Program.cs
@@ -10,31 +10,47 @@ namespace buoi1
     {
         static void Main(string[] args)
         {
-            Console.Title = "Selection Sort";
-            var numbers = new[] {10, 3, 1, 7, 9, 2, 0};
+            //Bài Mẫu của Thầy
+            //Console.Title = "Selection Sort";
+            //var numbers = new[] { 10, 3, 1, 7, 9, 2, 0 };
 
-            for (int i = 0; i < numbers.Length - 1; i++)
-            {
-                int m = i;
-                int maxValue = numbers[i];
+            //for (int i = 0; i < numbers.Length - 1; i++)
+            //{
+            //    int m = i;
+            //    int maxValue = numbers[i];
 
-                for (int j = i + 1; j < numbers.Length; j++)
-                {
-                    if (numbers[j].CompareTo(maxValue) > 0)
-                    {
-                        m = j;
-                        maxValue = numbers[j];
-                    }
-                }
+            //    for (int j = i + 1; j < numbers.Length; j++)
+            //    {
+            //        if (numbers[j].CompareTo(maxValue) > 0)
+            //        {
+            //            m = j;
+            //            maxValue = numbers[j];
+            //        }
+            //    }
 
-                int temp = numbers[i];
-                numbers[i] = numbers[m];
-                numbers[m] = temp;
+            //    int temp = numbers[i];
+            //    numbers[i] = numbers[m];
+            //    numbers[m] = temp;
+            //}
+            //Console.ReadKey();
 
-                
-            }
-            Console.ReadKey();
+            //Tính tổng của các phần tử trong mảng a có n số nguyên
+            int[] a = { 1, 2, 3, 4, 5 };
+            int sum = TinhTongMang(a);
+            Console.WriteLine("Tổng các phần tử trong mảng a là: " + sum);
+            Console.ReadLine();
+
+
         }
+
+        public static int TinhTongMang(int[] arr)
+        {
+            int sum = 0;
+            foreach (int number in arr)
+            {
+                sum += number;
+            }
+            return sum;
         }
     }
 }
